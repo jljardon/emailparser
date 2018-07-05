12aee1926ce8e113e482b507a4ecf4c3ad5a8a26
jljardon <github email address>
2018-07-05 19:17:06 -0400

    add functions to process emails and showTable
    Add functions to separate concerns withing the script.

--- index.php
+++ index.php
@@ -18,29 +18,43 @@
 
     <?php
       if (isset($_POST['submit'])) {
-          $emails = preg_split("/\r\n|\n\r|\s/", $_POST["emails"]);
-          $domains = [];
-          foreach ($emails as $key => $value) {
-              $domain = end(explode("@", $value));
-              if (strpos($value, "@") && !in_array($domain, $domains)) {
-                  array_push($domains, $domain);
+
+          // This function parses the emails separated by whitespace,
+          // iterates the array of emails, validates if they have an "@"
+          // and returns and array of unique email domains
+          function processEmails()
+          {
+              $emails = preg_split("/\r\n|\n\r|\s/", $_POST["emails"]);
+              $domains = [];
+              foreach ($emails as $key => $value) {
+                  $domain = end(explode("@", $value));
+                  if (strpos($value, "@") && !in_array($domain, $domains)) {
+                      array_push($domains, $domain);
+                  }
               }
+              return $domains;
           }
 
-          echo "<table>
+          // This function takes and array of domains as an argument
+          // Displays the list of domains inside a table in html.
+          function showTable($domains)
+          {
+              echo "<table>
                 <tr>
                   <th>Number</th>
                   <th>Domain</th>
                 </tr>";
 
-          foreach ($domains as $key => $value) {
-              echo "<tr>";
-              echo "<td>" . ($key + 1) . "</td>";
-              echo "<td>" . $value . "</td>";
-              echo "</tr>";
+              foreach ($domains as $key => $value) {
+                  echo "<tr>";
+                  echo "<td>" . ($key + 1) . "</td>";
+                  echo "<td>" . $value . "</td>";
+                  echo "</tr>";
+              }
+              echo "</table>";
           }
-
-          echo "</table>";
+          // Executes script
+          showTable(processEmails());
       }
      ?>
   </body>
